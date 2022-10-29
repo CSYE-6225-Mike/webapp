c0f05fa2d1bd7524d286ff052ceaeec072dce452
Michael <yan.chao@northeastern.edu>
2022-10-29 17:25:10 -0400

    Assignment 5

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const { syncBuiltinESMExports } = require('module')
 const app = express()
 const db = require('./config/db.sequelize')
-const userRoutes = require('./routes/route')
+const userRoutes = require('./routes/userRoute')
+const fileRoutes = require('./routes/fileRoutes')
 const bodyParser = require('body-parser')
 
 
@@ -12,7 +13,8 @@ app.use(express.urlencoded({ extended: true }))
 db.sync({ force: false }).then()
 
 app.use(userRoutes)
+app.use(fileRoutes)
 
-app.listen(8080, () => {
-    console.log("Web application is running at port: 8080!")
+app.listen(3000, () => {
+    console.log("Web application is running at port: 3000!")
 })
